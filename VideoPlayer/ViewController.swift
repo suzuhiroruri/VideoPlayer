583669519c2654041b4224b3653e6fc9ba6bf9e1
Hiromasa Suzuki <suzuhiroruri@gmail.com>
2019-12-10 18:18:55 +0900

    [add] import base files

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Quipper Ltd. on 9/16/19.
+//  Copyright © 2019 Quipper Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
