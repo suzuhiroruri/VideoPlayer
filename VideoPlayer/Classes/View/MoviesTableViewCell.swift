a29a8f92e37740921afbd68080215fa27ea5f4ed
suzuhiroruri <suzuhiroruri@gmail.com>
2019-12-10 22:43:13 +0900

    [add]list files

---
+++ VideoPlayer/Classes/View/MoviesTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesTableViewCell.swift
+//  VideoPlayer
+//
+//  Created by 鈴木 宏昌 on 2019/12/10.
+//  Copyright © 2019 Quipper Ltd. All rights reserved.
+//
+
+import UIKit
+
+class MoviesTableViewCell: UITableViewCell {
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+  }
+}
