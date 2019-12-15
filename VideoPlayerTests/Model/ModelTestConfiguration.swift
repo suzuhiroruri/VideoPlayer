b8dd28fd80fb4fa1dd1d34eacb939d12a43cdd3e
suzuhiroruri <suzuhiroruri@gmail.com>
2019-12-15 23:44:07 +0900

    [add] ModelTestConfiguration

---
+++ VideoPlayerTests/Model/ModelTestConfiguration.swift
@@ -0,0 +1,35 @@
+//
+//  ModelTestConfiguration.swift
+//  VideoPlayerTests
+//
+//  Created by Hiromasa Suzuki on 2019/12/15.
+//  Copyright © 2019 Hiromasa Suzuki. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+func uninitialized<T>() -> T { fatalError() }
+
+class ModelTestConfigration {
+  /// making json data from bundle file
+  ///
+  /// - Parameter jsonFileType: file type
+  /// - Returns: JSON?
+  static func getTestJSON(jsonFileType: ModelTestConfigration.JsonFileType) -> JSON? {
+    let testBundle = Bundle(for: TestStubs.self)
+    if let jsonPath = testBundle.path(forResource: jsonFileType.rawValue, ofType: "json") {
+      let url = URL(fileURLWithPath: jsonPath)
+      if let jsonData = try? Data(contentsOf: url) {
+        return try? JSON(data: jsonData)
+      }
+    }
+    return nil
+  }
+}
+
+extension ModelTestConfigration {
+  enum JsonFileType: String {
+    case videoEntityFactoryCreateArrayFromJSONObjectSuccess = "VideoEntityFactoryCreateArrayFromJSONObjectSuccess"
+  }
+}
